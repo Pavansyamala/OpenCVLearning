460c8f0e802dcfb0beb72e1d68112c175bd77fbb
Kasa Pavan <123885609+Pavansyamala@users.noreply.github.com>
2023-08-19 20:18:35 +0530

    Create adding_lines_video.py

---
+++ adding_lines_video.py
@@ -0,0 +1,23 @@
+import cv2
+import datetime
+
+cap = cv2.VideoCapture(0)
+
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT , 1080)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH , 1080)
+
+
+while cap.isOpened():
+
+    ret , frame = cap.read()
+    cv2.rectangle(frame,(0,110),(350,50),(0,255,0),4)
+    text = 'Time is  : ' + str(datetime.datetime.now())[11:19]
+
+    cv2.putText(frame , text , (10,100),cv2.FONT_HERSHEY_SIMPLEX , 1 , (255,0,255),2,cv2.LINE_AA)
+    cv2.imshow('Image' , frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break 
+
+cap.release()
+cv2.destroyAllWindows()
